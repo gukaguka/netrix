676ae89d88c004935ced88220b35fdeceeb64cf2
gukaguka <68656472+gukaguka@users.noreply.github.com>
2022-09-14 00:57:50 +0400

    Update backend.js

--- backend.js
+++ backend.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
-app.listen(port, () => {console.log(`Server is listening on port ${PORT}...`)})
+app.listen(port, () => {console.log(`Server is listening on port ${PORT}...`)});
 app.use(express.static('public'));
 var con = mysql.createConnection({  
   host: "us-cdbr-east-06.cleardb.net",  
