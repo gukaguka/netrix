19a39ca6ed704ec795fc249d92eb4c7a5ffd3a6d
gukaguka <68656472+gukaguka@users.noreply.github.com>
2022-09-14 00:44:15 +0400

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,4 +1,9 @@
 var mysql = require('mysql');  
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 80;
+app.listen(port, () => {console.log(`Server is listening on port ${PORT}...`)})
+app.use(express.static('public'));
 var con = mysql.createConnection({  
   host: "eu-cdbr-west-03.cleardb.net",  
   user: "bb818b55e33341",  
@@ -8,3 +13,14 @@ con.connect(function(err) {
   if (err) throw err;  
   console.log("Connected!");  
 });  
+
+app.post("/api", (request, response) => {
+  console.log("I got a request!");
+  console.log(request.body);
+  const data = request.body;
+  response.json({
+    status: "success",
+    query: data
+   
+  });
+});
